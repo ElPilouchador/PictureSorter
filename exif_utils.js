1039b9e4c2efc3fd8733ec1bb25628e0d404e21b
PLRenon <pierre-louis.renon@univ-tlse3.fr>
2023-05-07 20:54:31 +0200

    feat: Added function to return a Date object from exif data - if Date is not found, null is returned

--- exif_utils.js
+++ exif_utils.js
@@ -1,5 +1,4 @@
-
-const {ExifImage} = require("exif")
+const { ExifImage } = require("exif");
 
 function getExif(imagePath) {
     return new Promise(function (resolve, reject) {
@@ -18,4 +17,20 @@ function getExif(imagePath) {
         }
     });
 }
-module.exports = {getExif}
+
+async function getExifFormattedDate(imagePath) {
+    let final_date = null;
+    let re_date_str = "([0-9]{4}:[0-9]{2}:[0-9]{2}) ([0-9]{2}:[0-9]{2}:[0-9]{2})$";
+    let re_date = new RegExp(re_date_str);
+    let exif_data = await getExif(imagePath);
+    if (exif_data !== null) {
+        if (exif_data.exif.DateTimeOriginal !== undefined) {
+            //exif_data.exif.DateTimeOriginal
+            let match = exif_data.exif.DateTimeOriginal.match(re_date_str)
+            let unformattedDate = match[1].replaceAll(":","-")
+            final_date = new Date(Date.parse(unformattedDate))
+        }
+    }
+    return final_date;
+}
+module.exports = { getExif, getExifFormattedDate };
